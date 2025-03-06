3b81a770da8be24fb9f83ff8013ad4539a20de1d
czrptr <cezar.c.petriuc@gmail.com>
2025-03-06 22:11:37 +0200

    Serialize: Improvements

--- source/Serialize.hh
+++ source/Serialize.hh
@@ -1,69 +1,40 @@
+#include <algorithm>
 #include <vector>
-#include <utility>
 #include <type_traits>
 #include <span>
 
+#include <cstddef>
 #include <cstdint>
-#include <cstring>
+
+struct Serializer;
+struct Deserializer;
 
 template<typename T>
-std::vector<uint8_t> serialize(T const& t)
+constexpr auto descriptor_of()
 {
-  if constexpr (std::is_class_v<T>)
-  {
-    static_assert(false, "serialized must be specialized in order to be used");
-    std::unreachable();
-  }
-
-  size_t const size_of_write { sizeof(T) };
-  std::vector<uint8_t> buffer {};
-  buffer.reserve(size_of_write);
-
-  uint8_t const * begin = reinterpret_cast<uint8_t const*>(&t);
-  uint8_t const * end = begin + size_of_write;
-
-  buffer.insert(buffer.begin(), begin, end);
-  return buffer;
+  static_assert(false, "descriptor_of must be specialized in order to be used");
 }
 
 template<typename T>
-T deserialize(std::span<uint8_t> const& buffer)
+std::vector<uint8_t> serialize(T const& t);
+
+template<typename T>
+T deserialize(std::span<uint8_t const> buffer);
+
+template<typename T>
+T deserialize(std::vector<uint8_t> const& buffer)
 {
-  if constexpr (std::is_class_v<T>)
-  {
-    static_assert(false, "deserialized must be specialized in order to be used");
-    std::unreachable();
-  }
-
-  T result;
-
-  std::memcpy(
-    static_cast<void*>(&result),
-    static_cast<void const*>(buffer.data()),
-    sizeof(T));
-
-  return result;
+  return deserialize<T>(std::span(buffer.data(), buffer.size()));
 }
 
 struct Serializer final
 {
 public:
   template<typename T>
-  Serializer& write(T const& t)
+  void write(T const& t)
   {
     std::vector<uint8_t> const to_write = serialize(t);
-    m_buffer.insert(m_buffer.end(), to_write.begin(), to_write.end());
-    return *this;
-  }
-
-  template<typename... Args>
-  Serializer& write(Args&& ... args)
-  {
-    // call write on each argument
-    const auto unroller = [](...){};
-    unroller((write(std::forward(args)), 0)...);
-
-    return *this;
+    m_buffer.insert(m_buffer.end(), to_write.cbegin(), to_write.cend());
   }
 
   std::vector<uint8_t> result() const
@@ -75,95 +46,65 @@ private:
   std::vector<uint8_t> m_buffer {};
 };
 
+template<typename T>
+std::vector<uint8_t> serialize(T const& t)
+{
+  if constexpr (std::is_class_v<T>)
+  {
+    Serializer s;
+
+    std::apply([&](auto&& ... args) {
+      ((s.write(t.*args)), ...);
+    }, descriptor_of<T>());
+
+    return s.result();
+  }
+
+  uint8_t const * begin = reinterpret_cast<uint8_t const*>(&t);
+  uint8_t const * end = begin + sizeof(T);
+
+  return std::vector<uint8_t>(begin, end);
+}
+
 struct Deserializer final
 {
 public:
-  explicit Deserializer(std::span<uint8_t> buffer)
+  explicit Deserializer(std::span<uint8_t const> buffer)
     : m_buffer(buffer)
   {}
 
-  template<typename T>
-  T read()
+  template<typename T, typename MemberT>
+  void read(T& t, MemberT T::* member)
   {
-    T result = deserialize<T>(m_buffer.subspan(m_cursor));
-    m_cursor += sizeof(T);
-    return result;
+    t.*member = deserialize<MemberT>(m_buffer);
+    m_buffer = m_buffer.subspan(sizeof(MemberT));
   }
 
 private:
-  std::size_t m_cursor { 0u };
-  std::span<uint8_t> m_buffer;
+  std::span<uint8_t const> m_buffer;
 };
 
-/* ============================ */
-
-struct Data
+template<typename T>
+T deserialize(std::span<uint8_t const> buffer)
 {
-  char name[4];
-};
+  if constexpr (std::is_class_v<T>)
+  {
+    Deserializer d(buffer);
 
-struct Node
-{
-  char a;
-  int b;
-  float c;
-  Node* next;
-};
+    T result;
+    std::apply([&](auto&& ... args) {
+      ((d.read(result, args)), ...);
+    }, descriptor_of<T>());
 
-template<>
-std::vector<uint8_t> serialize<Node>(Node const& node)
-{
-  return Serializer{}
-    .write(node.a)
-    .write(node.b)
-    .write(node.c)
-    .write(node.next)
-    .result();
+    return result;
+  }
+
+  T result;
+
+  std::copy(
+    buffer.begin(),
+    buffer.end() + sizeof(T),
+    reinterpret_cast<uint8_t*>(&result));
+
+  return result;
 }
-
-template<>
-Node deserialize(std::span<uint8_t> const& buffer)
-{
-  Deserializer d(buffer);
-  return {
-    d.read<char>(),
-    d.read<int>(),
-    d.read<float>(),
-    d.read<Node*>(),
-  };
-}
-
-// int main()
-// {
-//     auto node1 = Node { 'a', 69, 420.0f, nullptr };
-//     auto node2 = Node { 'b', 32, 123.4f, &node1 };
-
-//     auto buffer = serialize(node2);
-//     std::cout << node2.a << " " << node2.b << " " << node2.c << " " << node2.next << "\n";
-
-//     for (auto const& byte : buffer)
-//     {
-//         std::cout << int(byte) << " ";
-//     }
-
-//     auto read = deserialize<Node>(buffer);
-//     std::cout << "\n" << read.a << " " << read.b << " " << read.c << " " << read.next;
-
-//     // T Node::*
-//     // auto ref_a = &Node::a;
-//     // node.*ref_a = 'b';
-
-//     // serialize(Data{});
-// }
-
-//   auto ref_a = &Node::a;
-//   node.*ref_a = 'b';
-
-//   get<&Node::a>(node);
-
-// template<typename ReturnT, typename ClassT, ReturnT ClassT::*member>
-// template<auto Member, typename T>
-// auto get(T const& t)
-// {
-//     return t.*Member;
-// }
